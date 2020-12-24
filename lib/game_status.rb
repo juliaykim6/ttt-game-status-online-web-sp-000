188b33e363ccdb3acfa3c86cc0c855e9fec09bb4
Julia Kim <juliaykim6@gmail.com>
2020-12-24 01:36:21 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -29,17 +29,17 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
-  win_index_1 = win_combination[0]
-  win_index_2 = win_combination[1]
-  win_index_3 = win_combination[2]
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
 
-  position_1 = board[win_index_1]
-  position_2 = board[win_index_2]
-  position_3 = board[win_index_3]
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
 
-  if position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
-  return win_combination
+  if position_1 == position_2 && position_2 == position_3 && position_taken?(board)
+    return win_combination 
+  end
 end
-end
-return false
+return false 
 end
