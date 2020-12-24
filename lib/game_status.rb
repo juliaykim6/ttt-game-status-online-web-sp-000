53fe71460b10cedb215d3cd1c3f1639e49a67ba1
Julia Kim <juliaykim6@gmail.com>
2020-12-24 01:23:19 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -14,3 +14,19 @@ WIN_COMBINATIONS = [
   [0,4,8],
   [2,4,6]
 ]
+
+def won?(board)
+  win_index_1 = win_combination[0]
+  win_index_2 = win_combination[1]
+  win_index_3 = win_combination[2]
+
+  position_1 = board[win_index_1]
+  position_2 = board[win_index_2]
+  position_3 = board[win_index_3]
+
+  if position_1 == position_2 && posit == position_3 && position_taken(board, win_index_1)
+  return win
+end
+end
+return false 
+end
